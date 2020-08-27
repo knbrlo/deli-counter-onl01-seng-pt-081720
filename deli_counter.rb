7834fe867ba1e99bf61f6ae58cd7c84cb69d6dcb
Ken Barlow <kenbrlow@gmail.com>
2020-08-27 22:44:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    final_string = "The line is currently:"
+    array.each_with_index do |name, index|
+      final_string << " #{index+1}. #{name}"
+    end
+    puts final_string
+  end
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  count = 0
+  while array.size > count
+    puts "Welcome, #{array[count]}. You are number #{count+1} in line."
+    count += 1
+  end
+
+end
