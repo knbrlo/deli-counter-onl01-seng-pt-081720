71747d77e03b0446c7da9b121d0381c3e70b2664
Ken Barlow <kenbrlow@gmail.com>
2020-08-27 22:56:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def now_serving(array)
   if array.size == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{array[0]}"
+    puts "Currently serving #{array[0]}."
     array.pop!
   end
 end
